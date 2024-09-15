3767090affe5684158585816b1d7168e4d8723d8
Flokapi <flokapi@pm.me>
2024-09-15 15:05:45 +0700

    Loading pages the static way for usability

--- app/routers/cnn.py
+++ app/routers/cnn.py
@@ -10,8 +10,8 @@ import app.cnn.cnn_prediction as cnn
 import app.operations.plot_predictions as plot
 
 
-router = APIRouter(prefix="/image-classification",
-                   tags=["Image Classification"])
+router = APIRouter(prefix="/evaluate",
+                   tags=["Evaluate"])
 
 
 async def run(fun, *args, **kwargs):
@@ -20,13 +20,13 @@ async def run(fun, *args, **kwargs):
 
 
 @router.get("/")
-@htmx("image_classification")
+@htmx("evaluate")
 async def hx_main(request: Request):
     pass
 
 
 @router.post("/hx-predict")
-@htmx("image_classification_result")
+@htmx("evaluate_result")
 async def hx_cnn_predict(request: Request, file: UploadFile = File(...)):
     file_content = await file.read()
     base64_image = await run(image.binary_to_base64, file_content)
